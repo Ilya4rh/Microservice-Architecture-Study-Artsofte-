7b336e5abbb8d407f3449cfd284d198eab622267
Ilya4rh <chernykn12ilya12@gmail.com>
2024-03-03 14:57:15 +0500

    N-слойная архитектура

---
+++ ProfileApi/Api/Controllers/User/Responses/UserInfoResponse.cs
@@ -0,0 +1,37 @@
+﻿namespace ProfileApi.Controllers.User.Responses;
+
+/// <summary>
+/// Для ответа на запрос о пользователе 
+/// </summary>
+public record UserInfoResponse
+{
+    /// <summary>
+    /// Индентификатор
+    /// </summary>
+    public required Guid Id { get; init; }
+
+    /// <summary>
+    /// Логин
+    /// </summary>
+    public required string Login { get; init; }
+
+    /// <summary>
+    /// Фамилия
+    /// </summary>
+    public required string LastName { get; init; }
+
+    /// <summary>
+    /// Имя
+    /// </summary>
+    public required string FirstName { get; init; }
+
+    /// <summary>
+    /// Адрес электронной почты
+    /// </summary>
+    public required string Email { get; init; }
+
+    /// <summary>
+    /// Роль
+    /// </summary>
+    public required string Role { get; init; }
+}
---
+++ ProfileApi/Api/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using Logic.Users.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using ProfileApi.Controllers.User.Requests;
+using ProfileApi.Controllers.User.Responses;
+using Logic.Users.Models;
+using Profile.Controllers.User.Responses;
+
+namespace ProfileApi.Controllers
+{
+    [Route("api/users")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserLogicManager userLogicManager;
+
+        [HttpGet]
+        [ProducesResponseType<UsersResponse>(200)]
+        public async Task<IActionResult> GetUsersAsync()
+        {
+            var users = await userLogicManager.GetUsersAsync();
+
+            return Ok(new UsersResponse { Users = users });
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType<UserInfoResponse>(200)]
+        public async Task<IActionResult> GetUserInfoAsync([FromQuery] Guid id)
+        {
+            var user = await userLogicManager.GetUserLogicAsync(id);
+
+            return Ok(new UserInfoResponse
+            {
+                Id = id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Login = user.Login,
+                Email = user.Email,
+                Role = user.Role
+            });
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(CreateUserResponse), 200)]
+        public async Task<ActionResult> CreateUserAsync([FromBody] CreateUserRequests createUserRequests)
+        {
+            var userId = await userLogicManager.CreateUserAsync(new UserLogic
+            {
+                Login = createUserRequests.Login,
+                FirstName = createUserRequests.FirstName,
+                LastName = createUserRequests.LastName,
+                Email = createUserRequests.Email,
+                Role = createUserRequests.Role
+            });
+
+            return Ok(new CreateUserResponse { Id = userId });
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(DeleteUsersResponse), 200)]
+        public async Task<ActionResult> DeleteUsersAsync([FromBody] DeleteUsersRequests deleteUsersRequests)
+        {
+            var users = await userLogicManager.GetRemainingUsersAsync(deleteUsersRequests.UsersId);
+
+            return Ok(new DeleteUsersResponse { Users = users });
+        }
+    }
+}
